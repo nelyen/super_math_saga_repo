7951cf634c02092c05abafd8d81079a004270ddf
nelyen <132521510+nelyen@users.noreply.github.com>
2023-11-15 14:59:18 -0800

    Create rand_sample_test.sql

---
+++ rand_sample_test.sql
@@ -0,0 +1,19 @@
+#standardSQL
+
+with
+
+tbl1 as (
+
+select
+playerid, 
+case 
+  when abtest_group = 'A' then 'control_group'
+  when abtest_group = 'B' then 'test_group' end as group_category,
+from king-ds-recruit-candidate-914.abtest.assignment 
+)
+
+select *
+from tbl1
+where rand() <= .3
+AND group_category = 'test_group'
+limit 385
